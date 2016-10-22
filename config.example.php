424492e1f0c9febb67447d4aca9918f2a0364668
Devon Sawatzky <d@dn3s.me>
2016-10-22 15:36:38 -0500

    quoted keys in default config file, fixes php7 compat

--- config.example.php
+++ config.example.php
@@ -1,10 +1,10 @@
 <?php
 $config=[
-	user => "username",
-	password => "mypass",
-	host => "localhost",
-	db => "my_database",
-	pst => 8,
-	gst => 5,
+	"user" => "username",
+	"password" => "mypass",
+	"host" => "localhost",
+	"db" => "my_database",
+	"pst" => 8,
+	"gst" => 5,
 ];
 ?>
