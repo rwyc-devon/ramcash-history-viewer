594020aad5dccf9a9c2a1d81109bd054c60086fd
Devon Sawatzky <d@dn3s.me>
2016-11-09 17:42:55 -0600

    ignore keypress events when the target is an <input> that way, the page isn't jumping around as you try to edit text

--- keyboard.js
+++ keyboard.js
@@ -4,11 +4,8 @@
 		"ArrowRight": document.getElementById("next-page"),
 		"ArrowLeft": document.getElementById("prev-page")
 	};
-	console.log(mappings);
 	window.addEventListener("keypress", function(e) {
-		console.log(e.key);
-		console.log(mappings[e.key]);
-		if(mappings[e.key]) {
+		if(e.target.tagName.toLowerCase()!="input" && mappings[e.key]) {
 			mappings[e.key].click();
 		}
 	});
